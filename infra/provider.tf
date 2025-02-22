79ef9f3b21ceec08ee6b4dcc62d92712c399d654
chinaza17 <chinazanwabuike17@gmail.com>
2025-02-22 12:41:33 +0000

    commit my changes

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                   = "eu-west-1"
-  shared_credentials_files = ["C:\Users\dell\Downloads\Adaugo_credentials.csv"]
+  shared_credentials_files = ["C:/Users/dell/Downloads/Adaugo_credentials.csv"]
 }
