2f097d85e0fb55ac81ff8bc0380ab0c8832f904a
chinaza17 <chinazanwabuike17@gmail.com>
2025-02-25 11:51:50 +0000

    commit changes

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -10,6 +10,7 @@ variable "ec2_sg_name_for_python_api" {}
 
 output "ssh_connection_string_for_ec2" {
   value = format("%s%s", "ssh -i /home/ubuntu/keys/aws_ec2_terraform ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
+}
 
 output "dev_proj_1_ec2_instance_id" {
   value = aws_instance.dev_proj_1_ec2.id
